4abd9f793163a9a5ce700c7c18af15ce4fdb7ff6
marsender <didier.corbiere@opale-concept.com>
2025-01-02 07:11:31 +0100

    Fix: incorrect total duration display

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -142,8 +142,8 @@ class BoardController extends AbstractController
 		// Add total duration
 		$footer = [];
 		$footer[] = '';
-		$info = sprintf('# Total: %s days (%s hours) %s€', number_format($totalDuration / 8, 2, ','), number_format($totalDuration, 1, ','), number_format($totalDuration * $this->boardDailyRate / 8, 2, ',', ''));
-		$info = str_replace([',00', ',0'], ['', ''], $info);
+		$info = sprintf('# Total: %s days (%s hours) %s €', number_format($totalDuration / 8, 2, ','), number_format($totalDuration, 1, ','), number_format($totalDuration * $this->boardDailyRate / 8, 2, ',', ''));
+		$info = str_replace([',00', ',0 '], ['', ''], $info);
 		$footer[] = $info;
 		$footer[] = '';
 
